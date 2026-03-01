176637ba0cf62411171faf2ece5381e3f74bf763
uo295721 <uo295721@uniovi.es>
2026-03-01 16:39:53 +0100

    Refactorización final: Eliminación de CiudadanoModelo y centralización completa de usuarios

--- src/main/java/modelo/UsuarioModelo.java
+++ src/main/java/modelo/UsuarioModelo.java
@@ -77,4 +77,19 @@ public class UsuarioModelo {
     	
     }
     
+    public CiudadanoDTO loginCiudadano(String identificador) {
+        // Definimos la SQL
+        String sql = "SELECT dni, nombre, email FROM Ciudadano WHERE dni = ? OR email = ?";
+        
+        // Ejecutamos la query. 
+        List<CiudadanoDTO> ciudadanos = db.executeQueryPojo(CiudadanoDTO.class, sql, identificador, identificador);
+        
+        //  Comprobamos si encontró a alguien
+        if (ciudadanos.isEmpty()) {
+            return null; // Si no existe
+        } else {
+            return ciudadanos.get(0); // Devolvemos el primero encontrado
+        }
+    }
+    
 }
